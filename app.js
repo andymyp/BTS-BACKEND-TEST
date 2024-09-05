652ca11e05ee8e51a783adc2db16543fe427fc62
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-09-05 10:51:53 +0700

    setup express and database

---
+++ app.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const database = require("./config/database");
+
+const app = express();
+
+//! Middlewares
+app.use(cors());
+app.use(express.json());
+
+//! Public Routes
+app.use("/status", (req, res) => {
+  res.status(200).json({
+    status: 1,
+    message: "Server is running",
+  });
+});
+
+//! Not Found
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Endpoint was not found" });
+});
+
+//! Connection
+const APP_PORT = process.env.APP_PORT || 3000;
+
+database.connect((error) => {
+  if (error) {
+    console.log("Database connection error:", error.message);
+    return;
+  }
+
+  console.log("Database connected");
+
+  app.listen(APP_PORT, () => {
+    console.log("App running in port:", APP_PORT);
+  });
+});
